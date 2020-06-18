c119d0eca270c4f1b0403a4f1f2c4f2edb371439
Devin_Moore <d.j.moore209@gmail.com>
2020-06-18 17:27:32 -0400

    sliding window is a little better

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,19 +4,35 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    # answers = []
+    # sub_Array = [0] * k
+    # iterations = 0
+    #
+    # for i in range(len(nums) - k + 1):
+    #     iterations = i
+    #     sub_Array_index = 0
+    #     sub_Array = [0] * k
+
+    #     for sub_index in range(len(sub_Array)):
+    #         sub_Array[sub_index] = nums[iterations]
+    #         iterations += 1
+
+    #     answers.append(max(sub_Array))
+
+    # place the inital sub array with the first values in num with the length of k
+    sub_Array = [nums[i] for i in range(k)]
+    # find the max in this thing and add it to the answer array
     answers = []
-    sub_Array = [0] * k
-    iterations = 0
+    answers.append(max(sub_Array))
 
-    for i in range(len(nums) - k + 1):
-        iterations = i
-        sub_Array_index = 0
-        sub_Array = [0] * k
-
-        for sub_index in range(len(sub_Array)):
-            sub_Array[sub_index] = nums[iterations]
-            iterations += 1
-
+    # loop through the number array
+    # stopping once the sub array is at the end of the nums list
+    for index in range(len(nums) - len(sub_Array)):
+        # pop the first index
+        sub_Array.pop(0)
+        # append to the sub array the value that the next value the sub array needs
+        sub_Array.append(nums[index + k])
+        # find the max value in this new sub array with the new value
         answers.append(max(sub_Array))
 
     return answers
